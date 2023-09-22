117beee32c5f1f79d0d7a8faa4165042770cd7b6
Chris Hartwig <chris@chris-hartwig.com>
2023-09-22 15:24:41 +0700

    fixed chain IdType map and added main shibarium

--- helpers/ChainIdDefTypeMap.ts
+++ helpers/ChainIdDefTypeMap.ts
@@ -2,5 +2,6 @@ export const chainIdDefaultIdTypeMap = new Map()
     .set(31337, '0x0212') // hardhat
     .set(80001, '0x0212') // polygon mumbai
     .set(137,   '0x0211') // polygon main 
-    .set(719,   '0x4203'); // shibarium puppynet
+    .set(109,   '0x0341') // shibarium main
+    .set(719,   '0x0342'); // shibarium puppynet
     
